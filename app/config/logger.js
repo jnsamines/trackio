b29b972af62ab1962eb12d7c682aec50c10c474d
jnsamines <jn.samines@gmail.com>
2014-05-21 16:06:53 -0600

    Modificada creacion de logger

--- app/config/logger.js
+++ app/config/logger.js
@@ -4,10 +4,10 @@
 
 var winston = require('winston');
 
-var logger = new (winston.Logger)({
+var logger = new winston.Logger({
     transports: [
-      new (winston.transports.Console)({ level: 'debug' }),
-      new (winston.transports.File)({ filename: './trackio.log' })
+      new winston.transports.Console({ level: 'debug' }),
+      new winston.transports.File({ filename: './trackio.log' })
     ]
 });
 
