b5801f6ca12ca844cc0678e76bc2aef383c91962
jnsamines <jn.samines@gmail.com>
2014-05-21 17:03:43 -0600

    Creados layout/vistas de tareas del menu de proyectos

--- public/assets/javascript/views/proyecto.js
+++ public/assets/javascript/views/proyecto.js
@@ -6,6 +6,16 @@ var dependencies = ['jquery', 'backbone', 'handlebars', 'collections/proyecto'];
 
 define(dependencies, function($, Backbone, Handlebars, ProyectoCollection){
 
+    // register helper
+    Handlebars.registerHelper('totalHorasProyecto',function(tareas, context){
+        var total = 0;
+        tareas.forEach(function(tarea){
+            total += tarea.tiempo;
+        });
+        return total;
+    });
+
+
     var ProyectoView = Backbone.View.extend({
         template : Handlebars.compile($('#proyectos_template').html()),
         el : '.table .group',
